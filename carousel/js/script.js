508c699d25b9c9882cf0887627489eb16cab36d6
BookOwl <stanleybookowl@gmail.com>
2015-10-13 18:11:18 -0400

    Fixed disappearing text bug

--- carousel/js/script.js
+++ carousel/js/script.js
@@ -26,6 +26,7 @@ function unfade(element) {
     var timer = setInterval(function () {
         if (op >= 1){
             clearInterval(timer);
+            element.style.display =  'initial';
         }
         element.style.opacity = op;
         element.style.filter = 'alpha(opacity=' + op * 100 + ")";
