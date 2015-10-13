6f096ce9f6ed48cd7e73cf90efd2156d3866edd5
BookOwl <stanleybookowl@gmail.com>
2015-10-13 12:08:52 -0400

    Added a new carousel gallery UI
    I created a new gallery carousel UI with Flikity. You can find it in the
    carousel folder. I also split up js/gallery.js into two files:
    images.js, which contains the image data, and gallery.js, which has
    everything else. This will make it easier to test out different gallery
    UIs.

---
+++ carousel/js/script.js
@@ -0,0 +1,83 @@
+var gallery = document.getElementById("gallery");
+var info = document.getElementById("info");
+
+gallery.innerHTML = "";
+for (var i = 0; i < images.length; i++){
+  gallery.innerHTML += "<div class='gallery-cell'><img src='." + images[i].new + "' alt='" + images[i].name + "' /></div>";
+  //console.log(gallery.innerHTML);
+}
+
+function fade(element) {
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 50);
+}
+
+function unfade(element) {
+    var op = 0.1;  // initial opacity
+    element.style.display = 'block';
+    var timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 10);
+}
+
+setTimeout( (function(){
+  var flkity = new Flickity('.gallery', {
+    wrapAround: true,
+    //freeScroll: true,
+    autoPlay: 4000 //milliseconds
+  });
+  
+  function ondragstart(){
+    fade(info);
+    info.innerHTML = '';
+  }
+  function onsettle(){
+    var i = flkity.selectedIndex;
+    info.innerHTML = '';
+    info.innerHTML += "<h1 class='arttitle'>" + images[i].name + "</h1>";
+    info.innerHTML += "<h2 class='artauther'>by " + images[i].author + "</h2>";
+    unfade(info);
+  }
+  onsettle();
+  flkity.on("dragStart", ondragstart);
+  flkity.on("cellSelect", ondragstart);
+  flkity.on("settle", onsettle);
+}),500);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
